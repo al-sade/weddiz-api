35761c39ee67d324c936a582e15cd756c4b34ddb
0xori <kadosh.ori@gmail.com>
2017-08-20 18:43:38 +0300

    added images array for each supplier album response added new key to env file

--- app/Http/Controllers/AlbumsController.php
+++ app/Http/Controllers/AlbumsController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Albums;
+use Log;
 
 class AlbumsController extends Controller
 {
@@ -12,7 +13,32 @@ class AlbumsController extends Controller
     }
 
     public function getAlbumsBySupplier($supplier_id){
-        $albums = Albums::find($supplier_id);
+        $albums = Albums::all()->where('supplier_id','=', $supplier_id);
+
+
+        //get Albums Images
+        $storagePath = env("LOCAL_UPLOADS_PATH");
+
+
+        Log::info('Showing env: '. env("LOCAL_UPLOADS_PATH"));
+        foreach ($albums as $key => $album)
+        {
+            $albumPath = $storagePath . "/albums/" . $supplier_id . "/" . $album["album_id"];
+            $albumImages = glob($albumPath. "/*.*");
+            $images = [];
+            foreach ($albumImages as $image){
+                $prefix = $storagePath;
+                // remove uploads dir prefix from image path
+                if (substr($image, 0, strlen($prefix)) == $prefix) {
+                    $image = substr($image, strlen($prefix));
+                    Log::info('Showing image path: '. $image);
+                    array_push($images, $image);
+                }
+            }
+            $album["images"] = $images;
+
+        }
+
         if($supplier_id){
             return $this->createSuccessResponse($albums, 200);
         }
